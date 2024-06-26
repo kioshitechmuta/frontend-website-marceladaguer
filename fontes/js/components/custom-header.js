9941053cc19f1c4b71efe3f583242ba57a754987
kioshitechmuta <163878642+kioshitechmuta@users.noreply.github.com>
2024-06-25 22:01:59 -0300

    Atualização do site.
    Atualização dos logos de menu, inclusão do botão voltar nas terapias

--- fontes/js/components/custom-header.js
+++ fontes/js/components/custom-header.js
@@ -15,12 +15,15 @@ class customHeader extends HTMLElement {
           </div>
           <nav id="navmenuitens">
             <ul class="headerUL">
-                <li class="itensmenu"><a href="../html/main.html"><span id="itemmenuhome" class="material-symbols-outlined">home</span>Início</a></li>
-                <li class="itensmenu"><a href="../html/quemsou.html"><span id="itemmenuhealthy" class="material-symbols-outlined">self_improvement</span>Marcela Daguer</a></li>
-                <li class="itensmenu"><a href="../html/terapias.html"><span id="itemmenuhealthy" class="material-symbols-outlined">digital_wellbeing</span>Terapias</a></li>
-                <li class="itensmenu"><a href="https://marceladaguer.wordpress.com/" target="_blank"><span id="itemmenuarticle" class="material-symbols-outlined">article</span>Blog</a></li>
-                <li class="itensmenu"><a href="https://wa.me/5511997111413" target="_blank"><span id="itemmenuarticle" class="material-symbols-outlined">chat</span>(11) 99711.1413</a></li>
-                <li class="itensmenu"><a href="mailto: marceladaguer@gmail.com" target="_blank"><span id="itemmenuarticle" class="material-symbols-outlined">alternate_email</span>marceladaguer@gmail.com</a></li>
+                <li><a href="../html/main.html"><span id="itemmenuhome" class="material-symbols-outlined">home</span>Início</a></li>
+                <li><a href="../html/quemsou.html"><span id="itemmenuhealthy" class="material-symbols-outlined">self_improvement</span>Marcela Daguer</a></li>
+                <li><a href="../html/terapias.html"><span id="itemmenuhealthy" class="material-symbols-outlined">digital_wellbeing</span>Terapias</a></li>
+                <li><a href="https://marceladaguer.wordpress.com/" target="_blank"><span id="itemmenuarticle" class="material-symbols-outlined">article</span>Blog</a></li>
+                <li class="itensmenu">
+                    <a href="https://www.instagram.com/marceladaguer?igsh=MWh6YWQyNzc0eGNqZw==" target="_blank"><i class="fa-brands fa-instagram logo"></i></a>&nbsp;&nbsp;&nbsp;
+                    <a href="https://wa.me/5511997111413" target="_blank"><i class="fa-brands fa-whatsapp logo"></i></a>&nbsp;&nbsp;&nbsp;
+                    <a href="mailto: marceladaguer@gmail.com" target="_blank"><i class="fa-solid fa-square-envelope logo"></i></a>
+                </li>
             </ul>
           </nav>
       </div>
@@ -33,8 +36,9 @@ class customHeader extends HTMLElement {
                     <li><a href="../html/terapias.html">Terapias</a></li>
                     <li><a href="https://marceladaguer.wordpress.com/" target="_blank">Blog</a></li>
                     <li>
-                        <a href="https://wa.me/5511997111413" target="_blank"><span class="material-symbols-outlined">chat</span></a>
-                        <a href="mailto: marceladaguer@gmail.com" target="_blank"><span class="material-symbols-outlined">alternate_email</span></a>
+                        <a href="https://www.instagram.com/marceladaguer?igsh=MWh6YWQyNzc0eGNqZw==" target="_blank"><i class="fa-brands fa-instagram logo"></i></a>&nbsp;&nbsp;&nbsp;
+                        <a href="https://wa.me/5511997111413" target="_blank"><i class="fa-brands fa-whatsapp logo"></i></a>&nbsp;&nbsp;&nbsp;
+                        <a href="mailto: marceladaguer@gmail.com" target="_blank"><i class="fa-solid fa-square-envelope logo"></i></a>
                     </li>
                 </ul>
             </nav>
